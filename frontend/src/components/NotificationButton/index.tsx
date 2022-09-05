1a21225c2538092266f9b0481f136f2da076a28a
Jonas Augusto <jonasaugusto99@gmail.com>
2022-09-05 12:55:32 -0300

    Other components

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import icon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+
+function NotificationButton() {
+  
+    return (
+      <div>
+        <img className="dsmeta-red-btn" src={icon} alt="Notificar"/>
+      </div>
+    )
+  }
+  
+  export default NotificationButton
+  
