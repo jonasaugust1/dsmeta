b3154b8dd44e1b5aecf904de39ee7341056356c4
Jonas Augusto <jonasaugusto99@gmail.com>
2022-09-07 09:59:04 -0300

    First Commit Base Backend Project

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -1,13 +1,26 @@
 package com.devsuperior.dsmeta;
 
+import com.devsuperior.dsmeta.entities.Department;
+import com.devsuperior.dsmeta.entities.Employee;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsmetaApplication {
+public class DsmetaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Department d1 = new Department(1L, "Vendas");
+		System.out.println(d1);
+
+		Employee e1 = new Employee(1L, "Maria", 6000.0, d1);
+		System.out.println(e1);
 	}
 
 }
