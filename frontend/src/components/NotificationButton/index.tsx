84e02cfcc80bbc31c6eeb16b18ac0cb2de69eb82
Jonas Augusto <jonasaugusto99@gmail.com>
2022-09-09 19:28:14 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,22 @@
 import icon from '../../assets/img/notification-icon.svg';
 import './styles.css';
+import axios from 'axios'
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+}
+
+function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+      .then(response => {})
+}
+
+function NotificationButton({saleId}: Props) {
   
     return (
-      <div>
-        <img className="dsmeta-red-btn" src={icon} alt="Notificar"/>
+      <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+        <img src={icon} alt="Notificar"/>
       </div>
     );
 };
